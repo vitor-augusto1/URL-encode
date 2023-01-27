3b426b3f24e621ce2eaef5a7d867c2b620c7f7a0
Vitor <vitor101bl9@gmail.com>
2023-01-27 14:49:47 -0300

    feat: Add constructor

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ import path from "path";
 import routes from "./routes";
 
 class App {
+  public express: express.Application;
+
+  public constructor() {
+    this.express = express();
+
+    this.middlewares();
+    this.routes();
+  }
 }
 
 export default new App().express;
