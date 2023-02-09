d2412c0c56b2488fee3f66d9466de60af70b1a1f
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-09 14:29:59 -0300

    refactor: Delete express view engine config line

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,6 @@ class App {
 
   private middlewares(): void {
     this.express.set("views", path.join(__dirname, "views"));
-    //this.express.set("view engine", "ejs");
     this.express.use('/public', express.static(path.join(__dirname, 'public')));
     this.express.use(express.json());
     this.express.use(cors());
