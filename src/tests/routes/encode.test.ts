fb7032a0b0fbe5ec5e66890536bd2acab7dd1954
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-13 11:28:29 -0300

    test: Create new function to perform api requests

--- src/tests/routes/encode.test.ts
+++ src/tests/routes/encode.test.ts
@@ -1,2 +1,11 @@
 import axios from "axios";
 
+const requestAPI = async () => {
+  const payload = {"raw-text":"http://localhost:3333"}
+  const response = axios.post(
+    "http://localhost:3333/encode",
+    payload
+  );
+  return response;
+}
+
