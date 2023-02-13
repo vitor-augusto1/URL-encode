40f67e269642bb1b531d79f366dc4a76e8712059
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-02-13 11:42:25 -0300

    test: Add test to check the equivalent encoded URL

--- src/tests/routes/encode.test.ts
+++ src/tests/routes/encode.test.ts
@@ -13,3 +13,9 @@ test("Should return 200 status code on successful encode", async () => {
   const response = await requestAPI();
   expect(response.status).toBe(200);
 })
+
+test("Should return the equivalent encoded URL", async () => {
+  const response = await requestAPI();
+  const encodedURL = encodeURIComponent("http://localhost:3333");
+  expect(response.data).toStrictEqual({success: encodedURL})
+})
